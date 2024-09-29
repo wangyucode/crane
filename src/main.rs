8eec5ce2bcd86da3de82b2061ecbb77b8a121cda
yu.wang <wangyu@wycode.cn>
2024-09-30 03:06:50 +0800

    simple http server

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
-use std::{env, fs};
-use std::io::{prelude::*, BufReader};
-use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::convert::Infallible;
+use std::env;
+use std::net::SocketAddr;
 
-fn main() {
+use http_body_util::Full;
+use hyper::body::{Bytes, Incoming};
+use hyper::header::HeaderValue;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let port = env::var("PORT").unwrap_or("8080".to_string());
     let addr = SocketAddr::from(([0, 0, 0, 0], port.parse().unwrap()));
+    let listener = TcpListener::bind(addr).await?;
     println!("Listening on port {}", addr);
-    // start hyper server
-    let listener = TcpListener::bind(addr).unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
 
-        handle_connection(stream);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = "Hello, World!";
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        // some other request
-    }
+async fn handler(_: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    let mut res = Response::new(Full::new(Bytes::from("Hello, World!")));
+    res.headers_mut().append("Server", HeaderValue::from_static("Crane"));
+    Ok(res)
 }
+
+
